890a06252f8a1653ded2e48d51e228b72b284ba5
michan00 <78699822+michan00@users.noreply.github.com>
2023-02-06 23:13:35 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <testing />
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 in CodeSandbox!" />
+</template>
+
+<script>
+import HelloWorldVue from "./components/HelloWorld.vue";
+import testing from "./components/testing.vue";
+export default {
+  name: "App",
+  components: {
+    HelloWorld: HelloWorldVue,
+    testing: testing,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/testing.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="head">
+    <ul>
+      <li>home</li>
+      <li>about</li>
+      <button>test</button>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "testing",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped>
+.head {
+  color: black;
+}
+li {
+  list-style-type: none;
+}
+button {
+  background-color: blue;
+}
+</style>
